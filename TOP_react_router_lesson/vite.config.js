5849753f0b241b460c57645ff7e746f6921e06f8
JustWaveThings <justwavethings@proton.me>
2023-05-20 17:15:57 -0400

    Update build params

--- TOP_react_router_lesson/vite.config.js
+++ TOP_react_router_lesson/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/TOP_react_router/',
+	base: '/TOP_react_router_lesson/',
 	plugins: [react()],
 });
