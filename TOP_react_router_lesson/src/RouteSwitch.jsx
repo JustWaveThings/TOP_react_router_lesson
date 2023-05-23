82657e11b6ef26264be06bd6dd828da50a99752e
JustWaveThings <justwavethings@proton.me>
2023-05-23 11:05:23 -0400

    Fix project file indentation

--- TOP_react_router_lesson/src/RouteSwitch.jsx
+++ TOP_react_router_lesson/src/RouteSwitch.jsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
-import Profile from './Profile';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Profile from "./Profile";
 
 const RouteSwitch = () => {
-	return (
-		<BrowserRouter basename="TOP_react_router_lesson">
-			<Routes>
-				<Route
-					path="/"
-					element={<App />}
-				/>
-				<Route
-					path="/profile"
-					element={<Profile />}
-				/>
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter basename="TOP_react_router_lesson">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default RouteSwitch;
--- TOP_react_router_lesson/vite.config.js
+++ TOP_react_router_lesson/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/TOP_react_router_lesson/',
-	plugins: [react()],
+  base: "/TOP_react_router_lesson/",
+  plugins: [react()],
 });
